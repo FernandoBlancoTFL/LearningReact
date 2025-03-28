72a35b43dad4b2e67aa7b729770e7fd1570da65c
TilFerLix <Fernando.blanco004@gmail.com>
2025-03-28 18:51:23 -0300

    movie search completed

--- projects/05-react-movie-search/src/App.jsx
+++ projects/05-react-movie-search/src/App.jsx
@@ -1,39 +1,90 @@
-import movieResponse from '../mocks/with-results.json'
-import withoutResults from '../mocks/no-results.json'
 import './css/App.css'
+import { useCallback, useEffect, useRef, useState } from 'react'
+import { useMovies } from './hooks/useMovies'
+import { Movies } from './components/Movies'
+import debounce from 'just-debounce-it'
+
+function useSearch () {
+  const [search, updateSearch] = useState('')
+  const [error, setError] = useState(null)
+  const isFirstInput = useRef(true)
+
+  useEffect(() => {
+    if (isFirstInput.current) {
+      isFirstInput.current = search === ''
+      return
+    }
+
+    if (search === '') {
+      setError('No se puede buscar una película vacía')
+      return
+    }
+
+    if (search.match(/^\d+$/)) {
+      setError('No se puede buscar una película con un número')
+      return
+    }
+
+    if (search.length < 3) {
+      setError('La búsqueda debe contener al menos 3 caracteres')
+      return
+    }
+
+    setError(null)
+  }, [search])
+
+  return { search, updateSearch, error }
+}
 
 export function App () {
-  const movies = movieResponse.Search
-  const hasMovies = movies?.length > 0
+  const [sort, setSort] = useState(false)
+  const { search, updateSearch, error } = useSearch()
+  const { movies, getMovies, loading } = useMovies({ search, sort })
+
+  const debouncedGetMovies = useCallback(
+    debounce(search => {
+      console.log(search)
+      getMovies({ search })
+    }, 300), [getMovies])
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    getMovies({ search })
+  }
+
+  const handleSort = () => {
+    setSort(!sort)
+  }
+
+  const handleChange = (event) => {
+    const newSearch = event.target.value
+    updateSearch(newSearch)
+    debouncedGetMovies(newSearch)
+  }
+
+  useEffect(() => {
+    console.log('getMovies received')
+  }, [getMovies])
 
   return (
     <div className='page'>
       <header>
         <h1>Buscador de películas</h1>
-        <form className='form'>
-          <input type='text' placeholder='Avengers, Star Wars, Sonic...' />
+        <form className='form' onSubmit={handleSubmit}>
+          <input
+            style={{
+              border: '1px solid transparent',
+              borderColor: error ? 'red' : 'transparent'
+            }} onChange={handleChange} value={search} type='text' placeholder='Avengers, Star Wars, Sonic...'
+          />
+          <input type='checkbox' onChange={handleSort} checked={sort} />
           <button type='submit'>Buscar</button>
         </form>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
       </header>
       <main>
         {
-            hasMovies
-              ? (
-                <ul>
-                  {
-                        movies.map(movie => (
-                          <li key={movie.imdbID}>
-                            <h3>{movie.Title}</h3>
-                            <p>{movie.Year}</p>
-                            <img src={movie.Poster} alt={movie.Title} />
-                          </li>
-                        ))
-                    }
-                </ul>
-                )
-              : (
-                <p>No se encontraron películas para esta búsqueda</p>
-                )
+          loading ? <p>Cargando...</p> : <Movies movies={movies} />
         }
       </main>
 
