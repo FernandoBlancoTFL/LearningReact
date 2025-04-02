3dacc134fbb380a0f3d97f5df0558790fdba8384
TilFerLix <Fernando.blanco004@gmail.com>
2025-04-02 01:26:55 -0300

    understanding how to use fetch and async/await functions

--- projects/06-practicing/01-cat-fact-image/src/service/facts.js
+++ projects/06-practicing/01-cat-fact-image/src/service/facts.js
@@ -1,10 +1,12 @@
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 
-export const getRandomFact = () => {
-  return fetch(CAT_ENDPOINT_RANDOM_FACT)
-    .then(res => res.json())
-    .then(response => {
-      const newFact = response.fact
-      return newFact
-    })
+export const getRandomFact = async () => {
+  try {
+    const res = await fetch(CAT_ENDPOINT_RANDOM_FACT)
+    if(!res.ok) throw new Error('An error occurred')
+    const response = await res.json()
+    return response.fact
+  } catch(error) {
+    console.log(error)
+  }
 }
